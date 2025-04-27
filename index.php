9cac2b47664d96cc1645185d290695fa0baaf0cd
dyahcandradewi123 <dyahcandradewi@student.ub.ac.id>
2025-04-27 11:28:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+require_once 'controller/MahasiswaController.php';
+
+$controller = new MahasiswaController();
+
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+switch ($action) {
+    case 'create':
+        $controller->create();
+        break;
+    case 'store': // <<<<<< INI YANG KURANG!!
+        $controller->store();
+        break;
+    case 'edit':
+        $controller->edit($_GET['id']);
+        break;
+    case 'delete':
+        $controller->delete($_GET['id']);
+        break;
+    default:
+        $controller->index();
+        break;
+}
+?>
+
 <h1>Data Mahasiswa</h1>
 <a href="index.php?action=create">Tambah Mahasiswa</a>
 <table border="1" cellpadding="10" cellspacing="0">
